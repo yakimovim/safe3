7e4977d761fffa8fda04bfb8737a4f63ace709bd
Ivan Iakimov <edlinsoft@gmail.com>
2022-10-12 15:00:41 +0300

    Icon selector control

--- Safe/ViewModels/EditStorageDetailsViewModel.cs
+++ Safe/ViewModels/EditStorageDetailsViewModel.cs
@@ -1,60 +1,25 @@
 ﻿using System;
-using System.Windows.Media;
 using EdlinSoftware.Safe.Domain;
 using EdlinSoftware.Safe.Events;
-using EdlinSoftware.Safe.Images;
 using EdlinSoftware.Safe.Storage.Model;
 using Prism.Commands;
 using Prism.Regions;
-using Prism.Services.Dialogs;
 
 namespace EdlinSoftware.Safe.ViewModels;
 
 public class EditStorageDetailsViewModel : ViewModelBase
 {
     private readonly IStorageInfoRepository _storageInfoRepository;
-    private readonly IIconsRepository _iconsRepository;
-
-    private string? _iconId;
-    public string? IconId
-    {
-        get => _iconId;
-        set => SetProperty(ref _iconId, value);
-    }
 
     public EditStorageDetailsViewModel(
-        IStorageInfoRepository storageInfoRepository,
-        IIconsRepository iconsRepository
+        IStorageInfoRepository storageInfoRepository
         )
     {
         _storageInfoRepository = storageInfoRepository ?? throw new ArgumentNullException(nameof(storageInfoRepository));
-        _iconsRepository = iconsRepository ?? throw new ArgumentNullException(nameof(iconsRepository));
 
         SaveChangesCommand = new DelegateCommand(OnSaveChanges, CanSaveChanges)
             .ObservesProperty(() => Title);
         CancelCommand = new DelegateCommand(OnCancel);
-        ClearIconCommand = new DelegateCommand(OnClearIcon);
-        SelectIconCommand = new DelegateCommand(OnSelectIcon);
-    }
-    private void OnSelectIcon()
-    {
-        DialogService.ShowDialog("IconsDialog", new DialogParameters(), result =>
-        {
-            if (result.Result == ButtonResult.OK)
-            {
-                var iconId = result.Parameters.GetValue<string>("IconId");
-
-                IconId = iconId;
-
-                Icon = _iconsRepository.GetIcon(IconId);
-            }
-        });
-    }
-
-    private void OnClearIcon()
-    {
-        IconId = null;
-        Icon = Icons.DefaultItemIcon;
     }
 
     private void OnSaveChanges()
@@ -92,11 +57,11 @@ public class EditStorageDetailsViewModel : ViewModelBase
         set => SetProperty(ref _description, value);
     }
 
-    private ImageSource _icon = Icons.DefaultItemIcon;
-    public ImageSource Icon
+    private string? _iconId;
+    public string? IconId
     {
-        get => _icon;
-        set => SetProperty(ref _icon, value);
+        get => _iconId;
+        set => SetProperty(ref _iconId, value);
     }
 
     public override void OnNavigatedTo(NavigationContext navigationContext)
@@ -111,10 +76,6 @@ public class EditStorageDetailsViewModel : ViewModelBase
     public DelegateCommand SaveChangesCommand { get; }
 
     public DelegateCommand CancelCommand { get; }
-
-    public DelegateCommand ClearIconCommand { get; }
-
-    public DelegateCommand SelectIconCommand { get; }
 
     private void Validate()
     {
