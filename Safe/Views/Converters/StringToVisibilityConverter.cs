c84525f5502feb672c75c9e5aef39005b7cc18f6
Ivan Iakimov <edlinsoft@gmail.com>
2022-10-17 10:51:17 +0300

    UI visibility

---
+++ Safe/Views/Converters/StringToVisibilityConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace EdlinSoftware.Safe.Views.Converters;
+
+public class StringToVisibilityConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (targetType != typeof(Visibility))
+            throw new NotSupportedException();
+        
+        if (value is string strValue)
+        {
+            return string.IsNullOrEmpty(strValue)
+                ? Visibility.Collapsed
+                : Visibility.Visible;
+        }
+
+        throw new NotSupportedException();
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotSupportedException();
+    }
+}
